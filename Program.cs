bf1316e9ed92c8e69df9506d498d5393c6d0bebf
SILAS-JOB <tiilas777@gmail.com>
2025-09-12 15:02:30 -0300

    start proj

---
+++ Program.cs
@@ -0,0 +1,12 @@
+var builder = WebApplication.CreateBuilder(args);
+var app = builder.Build();
+builder.Services.AddControllersWithViews();
+
+
+app.UseStaticFiles();
+app.MapControllers();
+app.UseRouting();
+
+
+
+app.Run();
